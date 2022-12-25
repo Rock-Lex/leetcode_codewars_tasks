a88745f72d3ddb24504633f574faebedba256e9e
Oleksandr Kamenskyi <okamenskyi@MacBook-Pro-von-Oleksandr.local>
2022-12-25 04:29:24 +0100

    Code wars taskt added

---
+++ codewars/CodeWars/CodeWars/CodeWars.h
@@ -0,0 +1,10 @@
+﻿// CodeWars.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
