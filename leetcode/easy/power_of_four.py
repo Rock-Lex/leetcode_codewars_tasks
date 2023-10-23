3f44389deea00eb32525958e96543adef94cfaf3
Oleksandr Kamenskyi <alex.kamenskyi@yahoo.com>
2023-10-23 20:31:39 +0200

    PowerOfFour, BestTimeToBuyAndSellStock - python

---
+++ leetcode/easy/power_of_four.py
@@ -0,0 +1,15 @@
+import math
+
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n > 0 and (math.log(n, 4) - int(math.log(n, 4)) == 0 or n == 1): return True
+        return False
+
+
+num = 8
+# num = 16
+# num = -16
+
+solution = Solution()
+print(solution.isPowerOfFour(num))
